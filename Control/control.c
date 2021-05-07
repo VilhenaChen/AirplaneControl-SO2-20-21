abd350a232368a75d31f2f812a9d417930b03a4c
VilhenaChen <diogodevilhena@sapo.pt>
2021-05-07 17:31:11 +0100

    Thread do Menu Principal Criacao da chaves no Registry com o Max de avioes e Aeroportos

--- Control/control.c
+++ Control/control.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <io.h>
 #include "estruturas.h"
+#define NTHREADS 1
 
 
 //Declaracao de Funcoes e Threads
@@ -18,7 +19,13 @@ int _tmain(int argc, TCHAR* argv[]) {
 	_setmode(_fileno(stdout), _O_WTEXT);
 	_setmode(_fileno(stderr), _O_WTEXT);
 #endif
-
+	
+	HANDLE hthreads[NTHREADS];
+	struct_dados dados;
+	HKEY chave; //Hanle para a chave depois de criada/aberta
+	DWORD resultado_chave; //inteiro long, indica o que aconteceu à chave, se foi criada ou aberta ou não
+	TCHAR nome_chave[TAM] = TEXT("SOFTWARE\\TEMP\\TP_SO2"), nome_par_avioes[TAM] = TEXT("maxAvioes"),nome_par_aeroportos[TAM] = TEXT("maxAeroportos");
+	DWORD par_valor_avioes = 10, par_valor_aeroportos = 10;
 
 	CreateMutex(0, FALSE, "Local\\$controlador$"); // try to create a named mutex
 	if (GetLastError() == ERROR_ALREADY_EXISTS) // did the mutex already exist?
@@ -28,8 +35,68 @@ int _tmain(int argc, TCHAR* argv[]) {
 	}
 	_tprintf(_T("--------------------- CONTROLADOR ---------------------\n"));
 
+	if (RegCreateKeyEx(HKEY_CURRENT_USER,
+		nome_chave /*caminho*/,
+		0,
+		NULL,
+		REG_OPTION_NON_VOLATILE /*opção default, funcionaria igual com 0*/,
+		KEY_ALL_ACCESS /*acesso total a todas as funcionalidades da chave*/,
+		NULL /*quem pode ter acesso, neste caso apenas o proprio*/,
+		&chave,
+		&resultado_chave)
+		!= ERROR_SUCCESS) /*diferente de sucesso*/
+	{
+		_tprintf(TEXT("ERRO! A chave não foi criada nem aberta!\n"));
+		return -1;
+	}
+	if (resultado_chave == REG_CREATED_NEW_KEY) {
+		_tprintf(TEXT("A chave %s foi criada!\n"), nome_chave);
+	}
+	else {
+		if (resultado_chave == REG_OPENED_EXISTING_KEY) {
+			_tprintf(TEXT("A chave %s foi aberta!\n"), nome_chave);
+		}
+	}
 
-	menu();
+	if (RegSetValueEx(
+		chave,		/*handle chave aberta*/
+		nome_par_avioes	/*nome parametro*/,
+		0,
+		REG_DWORD,
+		&par_valor_avioes, // para não dar warning
+		sizeof(par_valor_avioes)  //tamanho do que está escrito na string incluindo o /0
+	)!=ERROR_SUCCESS)
+	{
+		_tprintf(TEXT("ERRO ao aceder ao atributo %s !\n"),nome_par_avioes);
+		return -1;
+	}
+
+	if (RegSetValueEx(
+		chave,		/*handle chave aberta*/
+		nome_par_aeroportos	/*nome parametro*/,
+		0,
+		REG_DWORD,
+		&par_valor_aeroportos, // para não dar warning
+		sizeof(par_valor_aeroportos)  //tamanho do que está escrito na string incluindo o /0
+	)!=ERROR_SUCCESS)
+	{
+		_tprintf(TEXT("ERRO ao aceder ao atributo %s !\n"),nome_par_aeroportos);
+		return -1;
+	}
+
+	hthreads[0] = CreateThread(NULL, 0, Menu, &dados, 0, NULL);
+	if (hthreads[0] == NULL) {
+		_tprintf(_T("ERRO! Não foi possível criar a thread do menu!\n"));
+		return -1; 
+	}
+
+	WaitForMultipleObjects(NTHREADS, hthreads, FALSE, INFINITE);
+
+	for (int i = 0; i < NTHREADS; i++) {
+		CloseHandle(hthreads[i]);
+	}
+
+	RegCloseKey(chave);
 
 	return 0;
 }
@@ -88,5 +155,5 @@ DWORD WINAPI Menu(LPVOID param) {
 				Listar todos os aeroportos, aviões e passageiros
 		*/
 
-
+	return 0;
 }
