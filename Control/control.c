b5097390009420c436447e7a9508c3800f9da743
VilhenaChen <diogodevilhena@sapo.pt>
2021-05-08 18:50:12 +0100

    Criao de Aeroportos completa Listar Aeroportos Comunicacao entre Control e Aviao Iniciada

--- Control/control.c
+++ Control/control.c
@@ -10,6 +10,9 @@
 
 //Declaracao de Funcoes e Threads
 DWORD WINAPI Menu(LPVOID param);
+//DWORD WINAPI Comunicacao(LPVOID param);
+BOOL CriaAeroporto(TCHAR nome[TAM], int x, int y, struct_dados* dados);
+void Lista(struct_dados* dados);
 
 
 int _tmain(int argc, TCHAR* argv[]) {
@@ -27,14 +30,18 @@ int _tmain(int argc, TCHAR* argv[]) {
 	TCHAR nome_chave[TAM] = TEXT("SOFTWARE\\TEMP\\TP_SO2"), nome_par_avioes[TAM] = TEXT("maxAvioes"),nome_par_aeroportos[TAM] = TEXT("maxAeroportos");
 	DWORD par_valor_avioes = 10, par_valor_aeroportos = 10;
 
+	
+	dados.n_aeroportos_atuais = 0;
+	
 	CreateMutex(0, FALSE, "Local\\$controlador$"); // try to create a named mutex
 	if (GetLastError() == ERROR_ALREADY_EXISTS) // did the mutex already exist?
 	{
 		_tprintf(_T("ERRO! Já existe uma instância deste programa a correr!\n"));
 		return -1; // quit; mutex is released automatically
 	}
-	_tprintf(_T("--------------------- CONTROLADOR ---------------------\n"));
+	
 
+	//Criacao da Key onde serao guardados o max de avioes e Aeroportos
 	if (RegCreateKeyEx(HKEY_CURRENT_USER,
 		nome_chave /*caminho*/,
 		0,
@@ -84,11 +91,35 @@ int _tmain(int argc, TCHAR* argv[]) {
 		return -1;
 	}
 
+	//Criacao dos semaforos
+	/*semafAvioes = CreateSemaphore(NULL, 0, TAM, SEMAFORO_AVIOES);
+	if (semafAvioes == NULL) {
+		_tprintf(_T("Erro ao criar o semáforo Avioes!\n"));
+		return -1;
+	}
+	semafVazios = CreateSemaphore(NULL, TAM, TAM, SEMAFORO_VAZIOS);
+	if (semafVazios == NULL) {
+		_tprintf(_T("Erro ao criar o semáforo Vazios!\n"));
+		return -1;
+	}
+	*/
+	/*mutexControl = CreateMutex(NULL, FALSE, MUTEX_CONTROLADOR);
+	if (mutexControl == NULL) {
+		_tprintf(_T("Erro ao criar o mutex do produtor!\n"));
+		return -1;
+	}*/
+
+	//Criacao das Threads
 	hthreads[0] = CreateThread(NULL, 0, Menu, &dados, 0, NULL);
 	if (hthreads[0] == NULL) {
 		_tprintf(_T("ERRO! Não foi possível criar a thread do menu!\n"));
 		return -1; 
 	}
+	/*hthreads[1] = CreateThread(NULL, 0, Comunicacao, &dados, 0, NULL);
+	if (hthreads[0] == NULL) {
+		_tprintf(_T("ERRO! Não foi possível criar a thread da Comunicação!\n"));
+		return -1; 
+	}*/
 
 	WaitForMultipleObjects(NTHREADS, hthreads, FALSE, INFINITE);
 
@@ -102,8 +133,9 @@ int _tmain(int argc, TCHAR* argv[]) {
 }
 
 
-//Codigo de Funcoes e Threads
+//Codigo de Threads
 DWORD WINAPI Menu(LPVOID param) {
+	struct_dados* dados = (struct_dados*)param;
 	TCHAR com_total[TAM];
 	TCHAR com[TAM], arg1[TAM],arg2[TAM],arg3[TAM];
 	int cont;
@@ -124,7 +156,7 @@ DWORD WINAPI Menu(LPVOID param) {
 		com_total[_tcslen(com_total) - 1] = '\0';
 		cont = _stscanf_s(com_total, _T("%s %s %s %s"), com, (unsigned)_countof(com), arg1,(unsigned)_countof(arg1), arg2,(unsigned)_countof(arg2), arg3,(unsigned)_countof(arg3));
 		if (_tcscmp(com, _T("criar")) == 0 && cont == 4) {
-			
+			CriaAeroporto(arg1,_tstoi(arg2),_tstoi(arg3),dados);
 		}
 		else {
 			if (_tcscmp(com, _T("suspender")) == 0 && cont == 2) {
@@ -132,7 +164,7 @@ DWORD WINAPI Menu(LPVOID param) {
 			}
 			else {
 				if (_tcscmp(com, _T("lista")) == 0) {
-
+					Lista(dados);
 				}
 				else {
 					if (_tcscmp(com, _T("encerrar")) == 0) {
@@ -140,7 +172,7 @@ DWORD WINAPI Menu(LPVOID param) {
 					}
 					else {
 						_tprintf(_T("Comando Inválido!!!!\n"));
-									fflush(stdin);
+						fflush(stdin);
 					}
 				}
 			}
@@ -156,4 +188,73 @@ DWORD WINAPI Menu(LPVOID param) {
 		*/
 
 	return 0;
+}
+
+
+/*DWORD WINAPI Comunicacao(LPVOID param) {
+	int id_aviao;
+	struct_dados* dados = (struct_dados*) param;
+	struct_memoria_particular* ptrMemoriaParticular;
+	HANDLE objMapParticular;
+	struct_aviao_com comunicacao_particular;
+	//Lê da memoria partilhada geral
+
+	//Responde a cada aviao atraves da memoria partilhada particular
+	objMapParticular = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0,sizeof(struct_memoria_particular),(MEMORIA_AVIAO,id_aviao));
+	//Verificar se nao e NULL
+	if (objMapParticular == NULL) {
+		_tprintf(_T("Erro ao criar o File Mapping Particular!\n"));
+		return -1;
+	}
+
+	ptrMemoriaParticular = (struct_memoria_particular*)MapViewOfFile(objMapParticular, FILE_MAP_WRITE | FILE_MAP_READ, 0, 0, 0);
+	if (ptrMemoriaParticular == NULL) {
+		_tprintf(_T("Erro ao criar o ptrMemoriaParicular!\n"));
+		return -1;
+	}
+
+	// colocar em comunicacao particular o que é pretendido enviar
+	WaitForSingleObject(&dados->avioes[id_aviao].mutex, INFINITE);
+	CopyMemory(&ptrMemoriaParticular->aviao,&comunicacao_particular,sizeof(struct_aviao_com));
+	ReleaseMutex(&dados->avioes[id_aviao].mutex);
+}*/
+
+//Codigo de Funcoes
+
+BOOL CriaAeroporto(TCHAR nome[TAM], int x, int y, struct_dados* dados) {
+	if (dados->n_aeroportos_atuais < MAX_AEROPORTOS) {
+		if (x > TAM_MAP || x <= 0 || y > TAM_MAP || y <= 0) {
+			_tprintf(_T("Erro! O valor de X e Y deve ser entre 1 e 1000!!!\n"));
+			return FALSE;
+		}
+
+		for (int i = 0; i < dados->n_aeroportos_atuais; i++) {
+			if (_tcscmp(dados->aeroportos[i].nome, nome) == 0) {
+				_tprintf(_T("Erro! Os aeroportos devem ter nomes distintos!!!\n"));
+				return FALSE;
+			}
+			if ((dados->aeroportos[i].pos_x <= x+10 && dados->aeroportos[i].pos_x >= x-10) && (dados->aeroportos[i].pos_y <= y+10 && dados->aeroportos[i].pos_y >= y-10)) {
+				_tprintf(_T("Erro! Os aeroportos devem estar a uma distância de 10 posições!!!\n"));
+				return FALSE;
+			}
+		}
+		
+		_tcscpy_s(dados->aeroportos[dados->n_aeroportos_atuais].nome, _countof(dados->aeroportos[dados->n_aeroportos_atuais].nome), nome);
+		dados->aeroportos[dados->n_aeroportos_atuais].pos_x = x;
+		dados->aeroportos[dados->n_aeroportos_atuais].pos_y = y;
+		dados->n_aeroportos_atuais++;
+		return TRUE;
+	}
+	_tprintf(_T("Erro! Já foi atingido o máximo de aeroportos\n"));
+	return FALSE;
+}
+
+void Lista(struct_dados* dados) {
+	_tprintf(_T("Lista de Aeroportos\n"));
+	for (int i = 0; i < dados->n_aeroportos_atuais; i++) {
+		_tprintf(_T("%s\n"),dados->aeroportos[i].nome);
+		_tprintf(_T("\tCoordenada X: %d\n"),dados->aeroportos[i].pos_x);
+		_tprintf(_T("\tCoordenada Y: %d\n"),dados->aeroportos[i].pos_y);
+	}
+
 }
--- Control/estruturas.h
+++ Control/estruturas.h
@@ -7,6 +7,15 @@
 #include <io.h>
 #define TAM 200
 #define TAM_MAP 1000
+#define MAX_AVIOES 10
+#define MAX_AEROPORTOS 2
+#define MEMORIA_CONTROL _T("Memoria Control")
+#define MEMORIA_AVIAO _T("Memoria avião %d")
+#define MUTEX_AVIAO _T("Mutex do Aviao %d")
+#define SEMAFORO_AVIOES _T("Semaforo dos Avioes")
+#define SEMAFORO_VAZIOS _T("Semaforo dos Vazios")
+#define MUTEX_CONTROLADOR _T("Mutex do Controlador")
+
 
 typedef struct {
 	TCHAR nome[TAM];
@@ -15,16 +24,56 @@ typedef struct {
 } struct_aeroporto;
 
 typedef struct {
-	int id_processo;
+	int id;
 	int lotacao;
 	int velocidade;
 	int pos_x;
 	int pos_y;
 	struct_aeroporto* origem;
 	struct_aeroporto* destino;
+	HANDLE mutex;
 } struct_aviao;
 
 typedef struct {
-	struct_aeroporto* aeroportos;
-	struct_aviao* avioes;
+	struct_aeroporto aeroportos[MAX_AEROPORTOS];
+	struct_aviao avioes[MAX_AVIOES];
+	//struct_memoria_geral* ptrMemoriaGeral;
+	//HANDLE objMapGeral;
+	int n_aeroportos_atuais;
+	int n_avioes_atuais;
 } struct_dados;
+
+typedef struct {
+	TCHAR nome_origem[TAM];
+	TCHAR nome_destino[TAM];
+	int pos_x;
+	int pos_y;
+	int prox_x;
+	int prox_y;
+	int id_processo;
+	int lotacao;
+	int velocidade;
+	HANDLE mutex;
+}  struct_aviao_com;
+
+typedef struct {
+	int x_origem;
+	int y_origem;
+	int x_destino;
+	int y_destino;
+	BOOL avancar;
+	BOOL encerrar;
+}  struct_controlador_com;
+
+typedef struct {
+	struct_aviao_com aviao;
+} struct_memoria_particular;
+
+typedef struct {
+	struct_controlador_com controlador[TAM];
+	int in_control;
+	int out_control;
+	int nrControladores;
+	int nrAvioes;
+} struct_memoria_geral;
+
